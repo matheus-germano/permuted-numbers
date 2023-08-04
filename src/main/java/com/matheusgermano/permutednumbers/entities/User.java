e8ff27c202c976a44c7ff2ad26fa23aa7b3bf79d
Matheus Germano da Costa <dev.mgermano@gmail.com>
2023-08-04 15:39:43 -0300

    :recycle: refactor: using lombok builder to build user mock

--- src/main/java/com/matheusgermano/permutednumbers/entities/User.java
+++ src/main/java/com/matheusgermano/permutednumbers/entities/User.java
@@ -1,9 +1,17 @@
 package com.matheusgermano.permutednumbers.entities;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "user")
 public class User {

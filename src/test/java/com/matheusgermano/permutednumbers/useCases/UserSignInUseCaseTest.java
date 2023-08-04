c8e2d0432f3b3d66ea21817036afdfe905804de5
Matheus Germano da Costa <dev.mgermano@gmail.com>
2023-08-04 00:52:56 -0300

    :construction: UserSignInUseCase tests

--- src/test/java/com/matheusgermano/permutednumbers/useCases/UserSignInUseCaseTest.java
+++ src/test/java/com/matheusgermano/permutednumbers/useCases/UserSignInUseCaseTest.java
@@ -1,6 +1,7 @@
 package com.matheusgermano.permutednumbers.useCases;
 
 import com.matheusgermano.permutednumbers.entities.User;
+import com.matheusgermano.permutednumbers.protocols.IAuthAdapter;
 import com.matheusgermano.permutednumbers.protocols.ICryptoAdapter;
 import com.matheusgermano.permutednumbers.repositories.UsersRepository;
 import org.assertj.core.api.Assertions;
@@ -12,7 +13,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.security.NoSuchAlgorithmException;
 import java.util.Optional;
+import java.util.UUID;
 
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
@@ -25,10 +28,26 @@ public class UserSignInUseCaseTest {
     private UsersRepository usersRepository;
     @Mock
     private ICryptoAdapter cryptoAdapter;
+    @Mock
+    private IAuthAdapter authAdapter;
 
     @BeforeEach
-    public void setup(){
-        when(usersRepository.findByEmail("nonexistent@example.com")).thenReturn(Optional.of(new User()));
+    public void setup() throws NoSuchAlgorithmException {
+        String password = "123";
+        String encryptedPassword = cryptoAdapter.encrypt(password);
+
+        when(cryptoAdapter.encrypt(password)).thenReturn(encryptedPassword);
+        when(cryptoAdapter.matches(password, encryptedPassword)).thenReturn(true);
+        when(usersRepository.findByEmail("existent@example.com")).thenReturn(Optional.of(new User()));
+    }
+
+    @Test
+    @DisplayName("Should return a token when sign in is done successfully")
+    public void whenSignInIsDoneSuccessfully() {
+        String token = userSignInUseCase.execute("existent@example.com", "123");
+
+        Assertions.assertThat(token).isNotNull();
+        Assertions.assertThat(token).isNotBlank();
     }
 
     @Test
@@ -42,7 +61,7 @@ public class UserSignInUseCaseTest {
     }
 
     @Test
-    @DisplayName("Should throw an error if there is no user with provided e-mail")
+    @DisplayName("Should throw an error if there is no user with provided password")
     public void whenUserNotFoundWithProvidedPassword() {
         Assertions.assertThatExceptionOfType(Error.class).isThrownBy(()->
             userSignInUseCase.execute("nonexistent@example.com", "123")
@@ -50,8 +69,8 @@ public class UserSignInUseCaseTest {
     }
 
     @Test
-    @DisplayName("Should throw an error if cryptoAdapter is false")
-    public void withCryptoAdapterFails() {
+    @DisplayName("Should throw an error if raw password does not match with the hash")
+    public void whenRawPasswordDoesNotMatchWithHash() {
         when(cryptoAdapter.matches(anyString(), anyString())).thenReturn(false);
 
         Assertions.assertThatExceptionOfType(Error.class)

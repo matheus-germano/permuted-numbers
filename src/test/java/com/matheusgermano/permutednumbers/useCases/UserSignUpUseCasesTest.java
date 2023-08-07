7c631a67837cb87aef12d5b6220fb55343793dd1
Matheus Germano da Costa <dev.mgermano@gmail.com>
2023-08-07 14:37:58 -0300

    :white_check_mark: test: should throw an error if there is already a user with provided e-mail

--- src/test/java/com/matheusgermano/permutednumbers/useCases/UserSignUpUseCasesTest.java
+++ src/test/java/com/matheusgermano/permutednumbers/useCases/UserSignUpUseCasesTest.java
@@ -1,2 +1,38 @@
-package com.matheusgermano.permutednumbers.useCases;public class UserSignUpUseCasesTest {
+package com.matheusgermano.permutednumbers.useCases;
+
+import com.matheusgermano.permutednumbers.dtos.UserSignUpDTO;
+import com.matheusgermano.permutednumbers.entities.User;
+import com.matheusgermano.permutednumbers.mocks.UserMocks;
+import com.matheusgermano.permutednumbers.repositories.UsersRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Optional;
+
+import static org.mockito.Mockito.when;
+
+@ExtendWith(SpringExtension.class)
+public class UserSignUpUseCasesTest {
+    @InjectMocks
+    private UserSignUpUseCase userSignUpUseCase;
+    @Mock
+    private UsersRepository usersRepository;
+
+    @Test
+    @DisplayName("Should throw an error if there is already a user with provided e-mail")
+    public void whenProvidedEmailAlreadyExists() {
+        User existentUser = UserMocks.existentUser();
+        UserSignUpDTO invalidUserToSignUp = UserMocks.invalidUserToSignUp();
+
+        when(usersRepository.findByEmail(invalidUserToSignUp.getEmail())).thenReturn(Optional.of(existentUser));
+
+        Assertions.assertThatExceptionOfType(Error.class).isThrownBy(()->
+                userSignUpUseCase.execute(invalidUserToSignUp)
+        );
+    }
 }

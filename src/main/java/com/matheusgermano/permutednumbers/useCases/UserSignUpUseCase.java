285cc03ef702807f546bc863d541b9262b026c26
Matheus Germano da Costa <dev.mgermano@gmail.com>
2023-08-07 14:38:26 -0300

    :construction: signUp useCase

--- src/main/java/com/matheusgermano/permutednumbers/useCases/UserSignUpUseCase.java
+++ src/main/java/com/matheusgermano/permutednumbers/useCases/UserSignUpUseCase.java
@@ -1,2 +1,26 @@
-package com.matheusgermano.permutednumbers.useCases;public class UserSignUpUseCase {
+package com.matheusgermano.permutednumbers.useCases;
+
+import com.matheusgermano.permutednumbers.dtos.UserSignUpDTO;
+import com.matheusgermano.permutednumbers.entities.User;
+import com.matheusgermano.permutednumbers.repositories.UsersRepository;
+
+import java.util.Optional;
+
+public class UserSignUpUseCase {
+    private UsersRepository usersRepository;
+
+    public UserSignUpUseCase(UsersRepository usersRepository) {
+        this.usersRepository = usersRepository;
+    }
+
+    public Boolean execute(UserSignUpDTO userToSignUp) {
+        String userEmail = userToSignUp.getEmail();
+        Optional<User> foundUser = usersRepository.findByEmail(userEmail);
+
+        if (!foundUser.isEmpty()) {
+            throw new Error("There is already a user with this provided e-mail");
+        }
+
+        return true;
+    }
 }

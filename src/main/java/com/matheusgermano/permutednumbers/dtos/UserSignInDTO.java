a08280af54e41bbffe8cf998e9034c153bee08e1
Matheus Germano da Costa <dev.mgermano@gmail.com>
2023-08-08 19:20:33 -0300

    :construction: user controller

---
+++ src/main/java/com/matheusgermano/permutednumbers/dtos/UserSignInDTO.java
@@ -0,0 +1,31 @@
+package com.matheusgermano.permutednumbers.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserSignInDTO {
+    private String email;
+    private String password;
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ src/test/java/com/matheusgermano/permutednumbers/controllers/UserControllerTest.java
@@ -0,0 +1,44 @@
+package com.matheusgermano.permutednumbers.controllers;
+
+import com.matheusgermano.permutednumbers.dtos.SignInResponseDTO;
+import com.matheusgermano.permutednumbers.mocks.UserMocks;
+import com.matheusgermano.permutednumbers.useCases.UserSignInUseCase;
+import com.matheusgermano.permutednumbers.useCases.UserSignUpUseCase;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.security.NoSuchAlgorithmException;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(SpringExtension.class)
+public class UserControllerTest {
+    @InjectMocks
+    private UserController userController;
+    @Mock
+    private UserSignInUseCase userSignInUseCase;
+    @Mock
+    private UserSignUpUseCase userSignUpUseCase;
+
+    @BeforeEach
+    public void setup() throws NoSuchAlgorithmException {
+        when(userSignInUseCase.execute(any(), any())).thenReturn(anyString());
+    }
+
+    @Test
+    @DisplayName("Should return a object with auth token when sign in successfully")
+    public void whenUserSignInSuccessfully() throws NoSuchAlgorithmException {
+        SignInResponseDTO response = userController.signIn(UserMocks.validUserToSignIn()).getBody();
+
+        Assertions.assertThat(response.getToken()).isNotNull();
+        Assertions.assertThat(response.getToken()).isNotBlank();
+    }
+}
